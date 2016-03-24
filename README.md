12544d5f0cd259f0d602f8a4681dd3cbc0fab95b
Amael Noel <noelama@gmail.com>
2016-03-24 15:43:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# Config for WebStorm
+
+## Installing tsd
+
+```
+npm install typings --global
+typings  init
+```
+
+## Include Typing
+
+It helps typescript to know the keyword for angular. So the compiler does not generate errors.
+
+```
+typings install angular angular-material  --ambient --save-dev
+```
+
+## Bower
+
+We will create the configuration file for Bower and add the **angular-material** dependency.
+
+```
+bower init
+bower install angular-material --save
+```
+
+### refresh bower
+
+```
+bower update
+```
+
+### Include .bowerrc
+
+Need to create a file name *.bowerrc* and include the following text
+
+```
+{
+  "directory": "app/bower_components",
+  "scripts":{
+      "postinstall": "gulp wiredep"
+  }
+}
+```
+
+## Install typescript
+
+```
+npm install -g typescript
+npm update -g typescript
+```
+
+ use the following config in the *tsconfig.json*
+
+```
+{
+  "compilerOptions": {
+    "module": "commonjs",
+    "target": "es5",
+    "removeComments": true,
+    "sourceMap": true,
+    "emitDecoratorMetadata" :true,
+    "experimentalDecorators": true
+  },
+  "exclude": [
+    "node_modules",
+    "app/bower_components",
+    "typings/browser.d.ts",
+    "typings/browser"
+  ]
+}
+```
+
+
+## Internationalization
+
+ adds angular-translate library
+```
+bower install --save angular-translate messageformat
+```
+
+## Install Gulp
+
+ Install gulp globally
+```
+npm install -g gulp
+
+```
+
+Install gulp for a specific project locally
+```
+npm install --save-dev gulp
+```
+
+## Automatic include of dependencies
+
+Install Wiredep
+
+```
+npm install --save-dev wiredep gulp-inject
+```
---
+++ app/controller/mainController.ts
@@ -0,0 +1,143 @@
+/// <reference path="../boot.ts" />
+///<reference path="../model.ts"/>
+///<reference path="addUserDialogController.ts"/>
+///<reference path="contactPanelController.ts"/>
+
+module ContactManagerApp{
+    
+    export class MainController{       
+        static $inject =[
+            'userService',
+            '$mdSidenav',
+            "$mdToast",
+            "$mdDialog",
+            "$mdMedia",
+            "$mdBottomSheet"];
+                
+        constructor(private userService: IUserService,
+        private $mdSidenav : angular.material.ISidenavService,
+        private $mdToast: angular.material.IToastService,
+        private $mdDialog: angular.material.IDialogService,
+        private $mdMedia: angular.material.IMedia,
+        private $mdBottomSheet: angular.material.IBottomSheetService){
+            
+            var self = this;
+            this.userService
+            .loadAllUsers()
+            .then((x: User[])=>{
+                self.users = x;
+                self.selected = x[0];
+                self.userService.selectedUser = self.selected;
+                
+                console.log(self.users);
+            });          
+        }
+        
+        tabIndex: number =0;
+        searchText :string="";
+        users: User[]=[];  
+        selected : User =null;      
+        newNote:Note = new Note('',null);
+        
+        
+        selectUser (user:User):void {
+            this.selected=user;
+            this.userService.selectedUser = user;
+            
+            var sidenav = this.$mdSidenav('left');
+            if (sidenav.isOpen())
+            {
+                sidenav.close();
+            }
+            
+            this.tabIndex = 0;
+        }
+        
+        formScope:any;
+        
+        setFormScope(scope){
+            this.formScope = scope;
+        }
+        
+        addNote(){
+            this.selected.notes.push(this.newNote);
+            
+            // reset the form
+            this.formScope.noteForm.$setUntouched();
+            this.formScope.noteForm.$setPristine();
+            
+            this.newNote = new Note('',null);
+            this.openToast("Note added");
+        }
+        
+        clearNotes($event){
+            var confirm = this.$mdDialog.confirm()
+            .title("Are you sure you want to delete all notes?")
+            .textContent('All notes will be deleted, you can\'t undo this action.')
+            .targetEvent($event)
+            .ok("Yes")
+            .cancel("No");
+            
+            var self = this;
+            this.$mdDialog.show(confirm).then(()=>{
+                self.selected.notes = [];
+                self.openToast('Cleared notes');
+            })            
+        }
+        
+        showContactOptions($event):void {
+            this.$mdBottomSheet.show({
+                parent: angular.element(document.getElementById('wrapper')),
+                templateUrl: './view/contactSheet.html',
+                controller: ContactPanelController,
+                controllerAs: "cp",
+                bindToController:true,
+                targetEvent:$event
+            }).then((clickedItem)=>{
+                clickedItem && console.log(clickedItem.name + ' clicked!');
+            })
+        }
+        
+        removeNote(note:Note):void {
+            var foundIndex = this.selected.notes.indexOf(note);
+            this.selected.notes.splice(foundIndex,1);
+            this.openToast("Note was removed");
+        }
+        
+        openToast(message:string):void {
+           this.$mdToast.show(
+               this.$mdToast.simple()
+               .textContent(message)
+               .position('top right')
+               .hideDelay(3000)
+           );
+        }
+        
+        toggleSideNav() :void {
+            this.$mdSidenav('left').toggle();
+        }
+        
+        addUser($event){
+            var self = this;
+            var useFullScreen = (this.$mdMedia('sm') || this.$mdMedia('xs'));
+            
+            this.$mdDialog.show({
+                templateUrl: "./view/newUserDialog.html",
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                controller: AddUserDialogController,
+                controllerAs: 'ctrl',
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen
+            }).then((user:CreateUser)=>{
+                var newUser: User = User.fromCreate(user);
+                self.users.push(newUser);
+                self.selectUser(newUser);
+                self.openToast("User added");
+            },()=>{
+                console.log('You cancelled the dialog.');
+            });
+        }
+    }
+    
+}
